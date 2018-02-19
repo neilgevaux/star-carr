76453352ef6bf3577b9cf2c364901eab7bc00d2a
Neil Gevaux <neilgevaux@gmail.com>
2018-02-19 13:23:17 +0000

    intital-commit

---
+++ schools.php
@@ -0,0 +1,278 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="x-ua-compatible" content="ie=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="description" content="Welcome to the world of Star Carr: a Mesolithic site dating from 9000 BC">
+		<meta name="keywords" content="mesolithic, archaeology, milner, prehistoric, artefacts, stag, deer, frontlet, barbed, point, antler, ice age">
+		<meta name="robots" content="index, follow">
+		<meta name="revisit-after" content="3 month">
+		<meta name="author" content="">
+		
+		<title>Information for Schools | Star Carr</title>
+		 
+		<link rel="stylesheet" href="css/style.css">
+		<link rel="stylesheet" href="css/normalize.css">
+		<link rel="stylesheet" href="css/media.css">
+
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+
+		<link href='https://fonts.googleapis.com/css?family=Lora|Roboto:300,400,700' rel='stylesheet' type='text/css'>
+		<!-- <link rel="stylesheet" media="only screen and (max-width: 480px), only screen and (max-device-width: 480px)" href="mobile.css" /> -->
+        <!-- favicons -->
+		<link rel="shortcut icon" href="/favicon.ico">
+		<link rel="icon" sizes="16x16 32x32 64x64" href="images/favicons/icons/favicon.ico">
+		<link rel="icon" type="image/png" sizes="196x196" href="images/favicons/icons/favicon-192.png">
+		<link rel="icon" type="image/png" sizes="160x160" href="images/favicons/icons/favicon-160.png">
+		<link rel="icon" type="image/png" sizes="96x96" href="images/favicons/icons/favicon-96.png">
+		<link rel="icon" type="image/png" sizes="64x64" href="images/favicons/icons/favicon-64.png">
+		<link rel="icon" type="image/png" sizes="32x32" href="images/favicons/icons/favicon-32.png">
+		<link rel="icon" type="image/png" sizes="16x16" href="images/favicons/icons/favicon-16.png">
+		<link rel="apple-touch-icon" href="images/favicons/icons/favicon-57.png">
+		<link rel="apple-touch-icon" sizes="114x114" href="images/favicons/icons/favicon-114.png">
+		<link rel="apple-touch-icon" sizes="72x72" href="images/favicons/icons/favicon-72.png">
+		<link rel="apple-touch-icon" sizes="144x144" href="images/favicons/icons/favicon-144.png">
+		<link rel="apple-touch-icon" sizes="60x60" href="images/favicons/icons/favicon-60.png">
+		<link rel="apple-touch-icon" sizes="120x120" href="images/favicons/icons/favicon-120.png">
+		<link rel="apple-touch-icon" sizes="76x76" href="images/favicons/icons/favicon-76.png">
+		<link rel="apple-touch-icon" sizes="152x152" href="images/favicons/icons/favicon-152.png">
+		<link rel="apple-touch-icon" sizes="180x180" href="images/favicons/icons/favicon-180.png">
+		<meta name="msapplication-TileColor" content="#FFFFFF">
+		<meta name="msapplication-TileImage" content="/favicon-144.png">
+		<meta name="msapplication-config" content="/browserconfig.xml">
+		<!-- favicons -->
+
+	</head>
+
+	<body>
+		<!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+		<div id="wrapper"><!-- Page wrapper div -->
+			<div id="banner"><!-- Banner div -->
+				<div class="imgLogo"><!-- Logo container class -->
+					<a href="index.html">
+						<img src="images/global/customLogo.png" title="Home" width="150" height="50">
+					</a>
+				</div><!-- End logo container class -->
+
+					<ul class="shareButtons"><!-- Social media buttons class -->
+					  <li><a href="https://www.facebook.com/mesolithicstarcarr/?fref=ts" title="Share on Facebook" target="_blank"><img src="images/socialMedia/facebook50px.png" width="25" height="25"></a></li>
+					  <li><a href="https://plus.google.com/103574239071672153823/posts" target="_blank" title="Share on Google+"><img src="images/socialMedia/googleplus50px.png" width="25" height="25"></a></li>
+					  <li><a href="https://twitter.com/archaeostarcarr" target="_blank" title="Tweet"><img src="images/socialMedia/twitter62px.png" width="31" height="25"></a></li>
+					  <li><a href="https://www.youtube.com/channel/UCCLAO1KgN5v6rdq3Gg4xRLQ" target="_blank" title="Watch on YouTube"><img src="images/socialMedia/youtube72px.png" width="36" height="25" ></a></li>
+
+					</ul><!-- End social media buttons class -->
+
+			<div id="navbar"><!-- Navbar div -->	
+				<ul class="dropdown" onClick="return true"><!-- This enables hover on iOS -->
+					<li><a href="index.html">Home</a></li>
+					<li>About
+						<ul>
+							<li><a href="team.html">The Team</a></li>
+							<li><a href="history.html">History of Research</a></li>
+							<li><a href="vprt.html">Vale of Pickering Research Trust</a></li>
+							<li><a href="publications.html">Publications</a></li>	
+							<li><a href="news.html">News Stories</a></li>
+							<li><a href="funding.html">Funding</a></li>
+							<li><a href="contact.html">Contact</a></li>			
+						</ul>
+					<li><a href="gallery.html">Gallery</a></li>
+					<li>Explore
+						<ul>
+							<li><a href="pendant.html">Pendant</a></li>
+							<li><a href="headdresses.html">Headdresses</a>
+							<li><a href="fish.html">Fish Remains</a></li>
+							<li><a href="digital-museum.html">The Digital Museum</a></li>
+						</ul>
+					</li>
+					<li><a href="film.html">Film</a></li>
+					<li><a href="exhibitions.html">Exhibitions</a></li>
+					<li><a href="schools.html">Schools</a></li>
+					<li><a href="links.html">Links</a></li>
+				</ul>
+					
+					
+			</div><!-- End navbar div -->
+
+			</div><!-- End banner div -->
+	
+			<div id="mainContent"><!-- Main content div -->	
+				<div id="bannerSlide"><!-- Div for narrow image at top of page -->
+				<div id="bannerSlideshow">
+					 <img src="images/global/reconstructionBanner.jpg" title="" class="active" height="100" />
+   					 <img src="images/global/fieldworkBanner.jpg" title="" height="100"/>
+   					 <img src="images/global/fieldworkBanner2.jpg" title="" height="100"/>
+				</div>
+				</div><!-- End of narrow slideshow div -->
+			
+				<div id="columnContent"><!-- Div to contain content -->
+				<div class="newsPageContentLeft"><!-- Left page content div -->
+<!-- Heading box div -->
+				<h2 class="pageContent">Your message has been sent&mdash;thank you</h2>
+				
+				<!-- End heading box div -->
+				<!-- Confirmation container div  -->
+				<div id="confirmationContainer">
+				<?php
+					$feedback = $_POST['feedback'];
+					$other = $_POST['other'];
+					$name = $_POST['firstname'] . ' ' . $_POST['lastname'];
+					$email = $_POST['email'];
+					$institution = $_POST['institution'];
+
+					$to = 'education@starcarr.com';
+					$subject = 'Form submitted to Star Carr website';
+					$msg = "$name sent a new email: \n" .
+					"Feedback: $feedback\n" .
+					"Other suggestions: $other\n" .
+					"Institution: $institution";
+					mail($to, $subject, $msg, 'From:' . $email);
+
+					echo 'Thanks for submitting your details.<br /> <br />';
+					echo 'Your feedback: ' . $feedback . '<br /> <br />';
+					echo 'Your other suggestions: ' . $other . '<br /> <br />';
+					echo 'Your email address is ' . $email . '<br /> <br />';
+					echo 'Your institution is ' . $institution . '<br /> <br />';
+
+
+
+// if the url field is empty
+if(isset($_POST['url']) && $_POST['url'] == ''){
+
+    if($_POST["message"] == "" || $_POST["name"] == ""){
+        echo "<p></p>";
+    } else {
+        // then send the form to your email
+        mail( 'education@starcarr.com', 'Contact Form', print_r($_POST,true) );
+    }
+}
+
+// otherwise, let the spammer think that they got their message through
+
+				?>
+				</div>
+				<!-- End confirmation container div -->
+
+				</div><!-- End left page content div -->
+
+				<div class="newsPageContentRight"><!-- Right page content div -->
+				<h2 class="pageContent">Downloads</h2>	
+					<ul class="linkBox"><!-- The code for this style was inspired by http://teachinghistory100.org/objects/about_the_object/mesolithic_headdress -->
+					<h4 class="pageContent">All files</h4>
+						<li><a href="docs/ResourcesForSchools.zip" title="" target="_blank">Download all files</br>Zip archive (12.5mb)<img src="images/favicons/rightArrow.png"></a>
+						</li>
+						</br>
+					<h4 class="pageContent">11,000 Years Ago: Stories from the Middle Stone Age</h4>
+					<ul class="linkBox">
+						<li><a href="docs/11000YearsAgo.pdf" title="" target="_blank">11,000 Years Ago: Stories from the Middle Stone Age</br>PDF (532kb)<img src="images/favicons/rightArrow.png"></a>
+						</li>
+					<h3 class="pageContent">Supporting documents and worksheets</h3>
+					<h4 class="pageContent">Tools R Us</h4>
+					<li><a href="docs/ToolsRUs.pdf" title="" target="_blank">Tools R Us (Chapter 2, Activity 1)</br>PDF (2.9mb)<img src="images/favicons/rightArrow.png"></a>
+						</li>
+						</br>
+					<h4 class="pageContent">Which of these could you eat?</h4>
+					<li><a href="docs/WhichOfTheseCouldYouEat.pdf" title="" target="_blank">Which of these could you eat? (Chapter 3, Activity 1)</br>PDF (532kb)<img src="images/favicons/rightArrow.png"></a>
+						</li>
+					<li><a href="docs/WhichOfTheseCouldYouEatAnswers.pdf" title="" target="_blank">Answers</br>PDF (1.11mb)<img src="images/favicons/rightArrow.png"></a>
+						</li>
+						</br>
+					<h4 class="pageContent">Whose footprints?</h4>
+					<li><a href="docs/WhoseFootprints.pdf" title="" target="_blank">Whose footprints? (Chapter 3, Activity 1)</br>PDF (532kb)<img src="images/favicons/rightArrow.png"></a>
+						</li>
+						<li><a href="docs/WhoseFootprintsAnswersNames.pdf" title="" target="_blank">Answers (Names)</br>PDF (482kb)<img src="images/favicons/rightArrow.png"></a>
+						</li>
+						<li><a href="docs/WhoseFootprintsAnswersImages.pdf" title="" target="_blank">Answers (Images)</br>PDF (1.19mb)<img src="images/favicons/rightArrow.png"></a>
+						</li>
+						</br>
+					<h4 class="pageContent">Foods then and now</h4>
+					<li><a href="docs/FoodsThenAndNow.pdf" title="" target="_blank">Foods then and now (Chapter 3, Activity 2)</br>PDF (1.13mb)<img src="images/favicons/rightArrow.png"></a>
+						</li>
+					<li><a href="docs/FoodsThenAndNowAnswers.pdf" title="" target="_blank">Answers</br>PDF (1.12mb)<img src="images/favicons/rightArrow.png"></a>
+						</li>
+						</br>
+					<h4 class="pageContent">A Mesolithic picnic</h4>	
+					<li><a href="docs/A MesolithicPicnic.pdf" title="" target="_blank">A Mesolithic picnic (Chapter 3, Activity 3)</br>PDF (29kb)<img src="images/favicons/rightArrow.png"></a>
+						</li>
+					<li><a href="docs/AMesolithicPicnicExample.pdf" title="" target="_blank">Example</br>PDF (44kb)<img src="images/favicons/rightArrow.png"></a>
+						</li>
+					<li><a href="docs/AMesolithicPicnicInitialScoreSheet.pdf" title="" target="_blank">Intital score sheet</br>PDF (48kb)<img src="images/favicons/rightArrow.png"></a>
+						</li>	
+					<li><a href="docs/AMesolithicPicnicFullScoreSheet.pdf" title="" target="_blank">Full score sheet</br>PDF (44kb)<img src="images/favicons/rightArrow.png"></a>
+						</li>
+						</br>
+					<h4 class="pageContent">Pendant worksheet</h4>
+					<ul class="linkBox">
+						<li><a href="docs/pendant.doc" title="" target="_blank">Mesolithic shale pendant</br>Word doc (1.7mb)<img src="images/favicons/rightArrow.png"></a>
+						</li>
+					</ul>
+				</br>
+				</div><!-- End right page content div -->
+
+				</div><!-- End column content div -->
+				
+			</div><!-- End main content div -->
+			<div id="spacer"></div><!-- Spacer div-solves an issue with container height -->
+			<div id ="footer"><!-- Begin footer div -->
+							<div class="footerImg">
+								<ul class="footerLogos"><!-- Logo images -->
+								  <li><a href="https://erc.europa.eu/" title="European Research Council" target="_blank"><img src="images/global/ERC_Logo100px.png" height="35"></a></li>
+								  <li><a href="https://historicengland.org.uk/" title="Historic England" target="_blank"><img src="images/global/HistoricEngland100px.png" height="35"></a></li>
+								  <li><a href="http://www.york.ac.uk/" title="University of York" target="_blank"><img src="images/global/UniYork100px.png" height="35"></a></li>
+								  <li><a href="http://www.chester.ac.uk/" title="University of Chester" target="_blank"><img src="images/global/ChesterUni100px.png" height="35"></a></li>
+								  <li><a href="http://www.manchester.ac.uk/" title="The Univeristy of Manchester" target="_blank"><img src="images/global/ManchesterUni100px.png" height="35"></a></li>
+								  <li><a href="http://www.nerc.ac.uk/" title="NERC" target="_blank"><img src="images/global/nercLogo.png" height="35"></a></li>
+								  <li><a href="http://www.britac.ac.uk/" title="The British Academy" target="_blank"><img src="images/global/BritishAcademy100px.png" height="35"></a></li>
+								</ul><!-- End logo images -->
+							</div>
+			</div><!-- End footer div -->
+
+
+		</div><!-- End page wrapper div -->
+	</body>
+	<script type="text/javascript">
+		jQuery(document).ready( function($) {
+ 		jQuery('.dropdown').attr("onclick","return true");
+		});
+	</script>
+	<script>
+		function slideSwitch() {
+  		  var $active = $('#bannerSlideshow IMG.active');
+
+  		  if ( $active.length == 0 ) $active = $('#bannerSlideshow IMG:last');
+
+  		  // use this to pull the images in the order they appear in the markup
+  		  var $next =  $active.next().length ? $active.next()
+    	    : $('#bannerSlideshow IMG:first');
+
+   			 // uncomment the 3 lines below to pull the images in random order
+    
+   			 // var $sibs  = $active.siblings();
+    		// var rndNum = Math.floor(Math.random() * $sibs.length );
+  		  // var $next  = $( $sibs[ rndNum ] );
+
+
+   		 $active.addClass('last-active');
+
+  		  $next.css({opacity: 0.0})
+     	   .addClass('active')
+     	   .animate({opacity: 1.0}, 2000, function() {
+            $active.removeClass('active last-active');
+      	  });
+	}
+
+		$(function() {
+  	  setInterval( "slideSwitch()", 5000 );
+		});
+	</script>
+
+	<script type="text/javascript">
+			jQuery(document).ready( function($) {
+ 			jQuery('.dropdown').attr("onclick","return true");
+		});
+	</script>
+</html>
